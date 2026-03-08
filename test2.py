d1990ef5c2b3895552903216b061755a3e9eb3c3
ermukhan <e_ryskali@kbtu.kz>
2026-03-08 22:15:04 +0500

    eeee

--- test2.py
+++ test2.py
@@ -5,3 +5,8 @@ b=int(input("Enter another number: "))
 print(b)
 print("The sum is: ", a+b)
 print("The difference is: ", a-b)
+print("The product is: ", a*b)
+if b != 0:
+    print("The quotient is: ", a/b)
+else:
+    print("Error: Division by zero")
